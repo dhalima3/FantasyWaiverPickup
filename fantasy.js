fab95f38f2de80a48cf5b3358351ad6a854158c7
dhalima3 <dhalima@gatech.edu>
2015-12-22 05:00:36 -0500

    Format to use solely phantomjs and got past adding player screen

--- fantasy.js
+++ fantasy.js
@@ -1,37 +1,76 @@
-var league = "";
-var playerName = "";
-var playerToDropName = "";
+var steps = [];
+var testindex = 0;
+var loadInProgress = false;
+var page = require('webpage').create();
+var playerToAdd = "";
+var leagueName = "";
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36';
+page.settings.javascriptEnabled = true;
+page.settings.loadImages = false;//Script is much faster with this field set to false
+phantom.cookiesEnabled = true;
+phantom.javascriptEnabled = true;
 
-var casper = require('casper').create({
-    pageSettings: {
-        loadImages: false,//The script is much faster when this field is set to false
-        loadPlugins: false,
-        userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+steps = [
+    function() {
+        console.log("Step 1 - Open Home Page");
+        page.open(leagueName + playerToAdd, function(status) {
+
+        });
+    },
+
+    function() {
+        console.log("Step 2 - Fill in login form and submit it");
+        page.evaluate(function() {
+            document.getElementById("login-username").value= "";
+            document.getElementById("login-passwd").value= "";
+            document.getElementById("login-signin").click();
+        });
+    },
+
+    function() {
+        console.log("Step 3 - Click Add Player Button");
+        var addPlayerLink = page.evaluate(function() {
+            return document.getElementsByClassName("T-action-icon-add")[0].href;
+        });
+
+        page.open(addPlayerLink, function(status) {
+
+        });
+    },
+
+    function() {
+        console.log("Step 4 - Select Player to drop");
+        page.render('after.png');
+        //document.getElementsByClassName("login")[0].click();
+    },
+];
+
+interval = setInterval(executeRequestsStepByStep, 5000);
+
+function executeRequestsStepByStep(){
+    if (loadInProgress == false && typeof steps[testindex] == "function") {
+        steps[testindex]();
+        testindex++;
     }
-});
+    if (typeof steps[testindex] != "function") {
+        console.log("test complete!");
+        phantom.exit();
+    }
+}
 
-//First step is to open Yahoo
-casper.start().thenOpen(league, function() {
-    console.log("Yahoo Fantasy Page Opened");
-    this.capture('BeforeLogin.png');
-});
+/**
+ * These listeners are very important in order to phantom work properly. Using these listeners, we control loadInProgress marker which controls, weather a page is fully loaded.
+ * Without this, we will get content of the page, even a page is not fully loaded.
+ */
+page.onLoadStarted = function() {
+    loadInProgress = true;
+    console.log('Loading started');
+};
+page.onLoadFinished = function() {
+    loadInProgress = false;
+    console.log('Loading finished');
+};
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
 
-
-//Now we have to populate username and password, and submit the form
-casper.then(function(){
-    console.log("Login using username and password");
-    this.evaluate(function(){
-        document.getElementById("login-username").value= "";
-		document.getElementById("login-passwd").value= "";
-		document.getElementById("login-signin").click();
-    });
-});
-
-//Wait to be redirected to the Home page, and then make a screenshot
-casper.then(function(){
-    console.log("Make a screenshot and save it as AfterLogin.png");
-	this.wait(6000);//Wait a bit so page loads (there are a lot of ajax calls and that is why we are waiting 6 seconds)
-    this.capture('AfterLogin.png');
-});
-
-casper.run();
